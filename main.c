76e9b32048e8ee647ed43b4f1142eeb14d57d42a
User001 <adrian@hlozny.com>
2023-01-08 18:11:06 +0100

    ado committ

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include "server.h"
+#include "client.h"
+#include "string.h"
 
-int main() {
-    printf("Hello, World!\n");
-    printf("miro------\n");
+int main(int argc, char * argv[]) {
+    if (strcmp(argv[1],"server") == 0){
+        server(argc-1, argv+1);
+    } else {
+        client(argc-1, argv+1);
+    }
     return 0;
 }
